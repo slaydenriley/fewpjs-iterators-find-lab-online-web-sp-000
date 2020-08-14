3b61357b4fa59b27e444023c3987f41466a99929
Riley Slayden <slaydenriley@gmail.com>
2020-08-14 17:18:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const testVar = {}
+const testVar = {};
 
 function testFunc() {
   return "hi"
-}
+};
+
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
+};
